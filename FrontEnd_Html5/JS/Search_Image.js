ab7498b3aa55fae4b683eaa03cecf2d66176cadf
Srinath <venkata.mannam@st.ovgu.de>
2020-05-02 13:12:24 +0200

    Sweet_Alert_Implementation_1.1

--- FrontEnd_Html5/JS/Search_Image.js
+++ FrontEnd_Html5/JS/Search_Image.js
@@ -1,4 +1,4 @@
-define(['jquery'], function($) {
+define(['jquery', 'Libraries/SweetAlert/sweetalert2.all.min'], function($, swal) {
     $(function() {
 
         // store all the variables as an object properties
@@ -8,7 +8,8 @@ define(['jquery'], function($) {
             duplicateImageName: '',
             displayImageCharacterslen: 15,
             currentBrowserURL: $(location).attr('href'),
-            idDetails: ['#MainDiv', '#hideLabelId', '#fileUploadID', '#fileNameId', '#fileImgID', '#SearchBtn']
+            idDetails: ['#MainDiv', '#hideLabelId', '#fileUploadID', '#fileNameId', '#fileImgID', '#SearchBtn'],
+            supportedImageFormats: [".JPEG", ".PNG", ".BMP", ".TIFF", ".SVG", "EXIF", "JFIF"],
         };
 
         //When everything is loaded what to do first
@@ -19,7 +20,12 @@ define(['jquery'], function($) {
                 $(pageDetails.idDetails[0]).show();
                 $(pageDetails.idDetails[1]).hide();
             } catch (error) {
-                alert(JSON.stringify(error));
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: JSON.stringify(error)
+                });
             }
 
         });
@@ -28,8 +34,9 @@ define(['jquery'], function($) {
         $(pageDetails.idDetails[2]).change(function(e) {
             try {
                 if (this.files && this.files.length > 0) {
+                    pageDetails.imageName = e.target.value.split('\\').pop();
                     if (filesizevalidation(pageDetails.idDetails[2])) {
-                        pageDetails.duplicateImageName = pageDetails.imageName = e.target.value.split('\\').pop();
+                        pageDetails.duplicateImageName = pageDetails.imageName;
                         if (pageDetails.imageName.length > pageDetails.displayImageCharacterslen) {
                             pageDetails.duplicateImageName = pageDetails.imageName.slice(0, 4) + '....' + pageDetails.imageName.slice(pageDetails.imageName.length - 6, pageDetails.imageName.length)
                         }
@@ -37,11 +44,20 @@ define(['jquery'], function($) {
                         $(pageDetails.idDetails[1]).show();
 
                     } else {
-                        alert("Invalid File! Please select a valid file");
+                        swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Invalid File! Please select a valid file!'
+                        });
                     }
                 }
             } catch (error) {
-                alert(JSON.stringify(error));
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: JSON.stringify(error)
+                });
             }
         });
 
@@ -52,7 +68,12 @@ define(['jquery'], function($) {
                 $(pageDetails.idDetails[1]).hide();
                 pageDetails.duplicateImageName = pageDetails.imageName = '';
             } catch (error) {
-                alert(JSON.stringify(error));
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: JSON.stringify(error)
+                });
             }
         });
 
@@ -60,29 +81,48 @@ define(['jquery'], function($) {
         $(pageDetails.idDetails[5]).click(function() {
             try {
                 if ($(pageDetails.idDetails[2]).val() == '' || $(pageDetails.idDetails[2]).val() == null || $(pageDetails.idDetails[2]).val() == undefined) {
-                    alert("Please select an image");
+                    swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Please select an image!'
+                    });
                 } else {
                     console.log("Proceed further");
                 }
             } catch (error) {
-                alert(JSON.stringify(error));
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: JSON.stringify(error)
+                });
             }
         });
 
         //this function will validate the file whether it is an image or not and valid file or not
         function filesizevalidation(fileuploadid) {
-            var fileMinSize = 0;
-            var fileValidation = false;
-            var fileInput = $(fileuploadid);
-            for (var i = 0; i < fileInput[0].files.length; i++) {
-                if (fileInput[0].files[i].size > fileMinSize) {
-                    fileValidation = true;
-                } else {
-                    fileValidation = false;
-                    break;
+            try {
+                var fileMinSize = 0;
+                var fileValidation = false;
+                var fileInput = $(fileuploadid);
+                for (var i = 0; i < fileInput[0].files.length; i++) {
+                    if (fileInput[0].files[i].size > fileMinSize) {
+                        fileValidation = ((pageDetails.supportedImageFormats.indexOf((pageDetails.imageName.match(/\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gmi)[0]).toUpperCase())) >= 0 ? true : false);
+                    } else {
+                        fileValidation = false;
+                        break;
+                    }
                 }
+                return fileValidation;
+
+            } catch (error) {
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: JSON.stringify(error)
+                });
             }
-            return fileValidation;
         }
 
     });
