3ca9f6e7dca49c6fa450fc49004944b33bb749f7
Srinath <venkata.mannam@st.ovgu.de>
2020-05-19 11:52:40 +0200

    19_May_2020_V1

--- FrontEnd_Html5/Scripts/CustomScripts/Search_Image.js
+++ FrontEnd_Html5/Scripts/CustomScripts/Search_Image.js
@@ -1,5 +1,5 @@
-define(['jquery', 'jqueryui', 'sweetalert', 'datatables'], function($, jqueryui, swal, datatables) {
-    $(function() {
+define(['jquery', 'jqueryui', 'sweetalert', 'datatables'], function ($, jqueryui, swal, datatables) {
+    $(function () {
 
         // store all the variables as an object properties
         var pageDetails = {
@@ -12,8 +12,30 @@ define(['jquery', 'jqueryui', 'sweetalert', 'datatables'], function($, jqueryui,
             supportedImageFormats: [".JPG", ".JPEG", ".PNG", ".BMP", ".TIFF", ".SVG", "EXIF", "JFIF"],
         };
 
+        var tableData = [{
+            ID: 0,
+            imageURL: 'imagesData/Sample1.jpg',
+            imageDetails: 'Dog'
+        }, {
+            ID: 1,
+            imageURL: 'imagesData/Sample2.jpg',
+            imageDetails: 'Butterfly'
+        }, {
+            ID: 2,
+            imageURL: 'imagesData/Sample3.jpg',
+            imageDetails: 'Parrot'
+        }, {
+            ID: 3,
+            imageURL: 'imagesData/Sample4.jpg',
+            imageDetails: 'Nature'
+        }, {
+            ID: 4,
+            imageURL: 'imagesData/Sample5.jpg',
+            imageDetails: 'Riding Bycycle'
+        }];
+
         //When everything is loaded what to do first
-        $(document).ready(function() {
+        $(document).ready(function () {
             try {
                 //loader before loading the data
                 // $('body').addClass("loading");
@@ -31,7 +53,7 @@ define(['jquery', 'jqueryui', 'sweetalert', 'datatables'], function($, jqueryui,
         });
 
         //detect change events when the user browses for an image
-        $(pageDetails.idDetails[2]).change(function(e) {
+        $(pageDetails.idDetails[2]).change(function (e) {
             try {
                 if (this.files && this.files.length > 0) {
                     pageDetails.imageName = e.target.value.split('\\').pop();
@@ -62,7 +84,7 @@ define(['jquery', 'jqueryui', 'sweetalert', 'datatables'], function($, jqueryui,
         });
 
         //remove the file context when the user clicks on remove image symbol and hide
-        $(pageDetails.idDetails[4]).click(function() {
+        $(pageDetails.idDetails[4]).click(function () {
             try {
                 $(pageDetails.idDetails[2]).val('');
                 $(pageDetails.idDetails[1]).hide();
@@ -82,7 +104,7 @@ define(['jquery', 'jqueryui', 'sweetalert', 'datatables'], function($, jqueryui,
         });
 
         //On click of the search button
-        $(pageDetails.idDetails[5]).click(function() {
+        $(pageDetails.idDetails[5]).click(function () {
             try {
                 if ($(pageDetails.idDetails[2]).val() == '' || $(pageDetails.idDetails[2]).val() == null || $(pageDetails.idDetails[2]).val() == undefined) {
                     swal.fire({
@@ -91,34 +113,16 @@ define(['jquery', 'jqueryui', 'sweetalert', 'datatables'], function($, jqueryui,
                         text: 'Please select an image!'
                     });
                 } else {
-                    var tableData = [{
-                        ID: 0,
-                        imageURL: 'imagesData/Sample1.jpg',
-                        imageDetails: 'Dog'
-                    }, {
-                        ID: 1,
-                        imageURL: 'imagesData/Sample2.jpg',
-                        imageDetails: 'Butterfly'
-                    }, {
-                        ID: 2,
-                        imageURL: 'imagesData/Sample3.jpg',
-                        imageDetails: 'Parrot'
-                    }, {
-                        ID: 3,
-                        imageURL: 'imagesData/Sample4.jpg',
-                        imageDetails: 'Nature'
-                    }, {
-                        ID: 4,
-                        imageURL: 'imagesData/Sample5.jpg',
-                        imageDetails: 'Riding Bycycle'
-                    }];
 
                     if ($.fn.DataTable.isDataTable('#irtexdbID')) {
                         pageDetails.tabledetails.destroy();
                     }
 
                     CreateDatatable('irtexdbID', tableData);
+                    $("[id^='accordion']").accordion({ header: "h3", collapsible: true, active: false, heightStyle: "content" });
                     $('#irtexdbID').show();
+                    $("#irtexdbID").css("width", "100%");
+
                 }
             } catch (error) {
                 swal.fire({
@@ -157,46 +161,71 @@ define(['jquery', 'jqueryui', 'sweetalert', 'datatables'], function($, jqueryui,
         }
 
 
+        // detect when the accoirdion is clicked and collapse all other than the current one
+        $(document).on('click', '[id^="accordion"]', function () {
+            try {
+                $("[id^='accordion']:not(#" + this.id + ")").accordion({ header: "h3", collapsible: true, active: false, heightStyle: "content" });
+
+            } catch (error) {
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: JSON.stringify(error)
+                });
+            }
+        });
+
+
         //to create the data table
         function CreateDatatable(TableName, BindingData) {
             var deferred = $.Deferred();
             try {
                 pageDetails.tabledetails = $('#' + TableName).DataTable({
-                    dom: 'Blfrtip',
+                    dom: 'lrtip',
                     data: BindingData,
                     "deferRender": true,
                     columns: [{
-                            "className": 'CustomClass',
-                            "orderable": false,
-                            "data": null,
-                            render: function(data, type, row) {
-                                return '<input type="checkbox" id="selectIndex' + row.ID + '">';
-                            }
-                        },
-                        {
-                            "className": 'CustomClass',
-                            "orderable": false,
-                            "data": null,
-                            render: function(data, type, row) {
-                                return '<label>' + (row.ID + 1) + '</label>';
-                            }
-                        },
-                        {
-                            "data": "imageURL",
-                            "className": 'normalClass',
-                            render: function(data, type, row) {
-                                return '<img class="tableImgClass" src="' + data + '"/>';
-                            },
-                            "orderable": false
-                        },
-                        {
-                            "data": "imageDetails",
-                            "className": 'normalClass',
-                            render: function(data, type, row) {
-                                return data;
-                            },
-                            "orderable": false
+                        "className": 'CustomClass',
+                        "orderable": false,
+                        "data": null,
+                        render: function (data, type, row) {
+                            return '<input type="checkbox" id="selectIndex' + row.ID + '">';
                         }
+                    },
+                    {
+                        "className": 'CustomClass',
+                        "orderable": false,
+                        "data": null,
+                        render: function (data, type, row) {
+                            return '<label>' + (row.ID + 1) + '</label>';
+                        }
+                    },
+                    {
+                        "data": "imageURL",
+                        "className": 'thirdColumnClass',
+                        render: function (data, type, row) {
+                            return '<img class="tableImgClass" src="' + data + '"/>';
+                        },
+                        "orderable": false
+                    },
+                    {
+                        "data": "imageDetails",
+                        "className": 'lastCOlumnClass',
+                        render: function (data, type, row) {
+                            return '<div style:width:100%;>' +
+                                '<div id="accordion' + (row.ID + 1) + '">' +
+                                '<h3>Explain</h3>' +
+                                '<div style="height: auto !important;">' +
+                                '<p>' + data + '</p>' +
+                                '<p><input class="VisualBtnClass" type="button" value="Visual Explanation" id="VisualBtn' + (row.ID + 1) + '"></p>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>';
+                        },
+                        "autoWidth": false,
+                        "orderable": false,
+                    }
                     ],
                     // "aaSorting": [[1, 'dsc']],
                     "lengthMenu": [
@@ -207,16 +236,50 @@ define(['jquery', 'jqueryui', 'sweetalert', 'datatables'], function($, jqueryui,
 
                 deferred.resolve();
             } catch (err) {
-                // swal.fire({
-                //     icon: 'error',
-                //     title: 'Oops...',
-                //     text: 'Something went wrong!',
-                //     footer: JSON.stringify(error)
-                // });
-                console.log(JSON.stringify(error));
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: JSON.stringify(error)
+                });
                 deferred.reject();
             }
             return deferred.promise();
+        }
+
+        // detect when the accoirdion is clicked and collapse all other than the current one
+        $(document).on('click', '[id^="VisualBtn"]', function () {
+            try {
+                var getcurrentid = this.id;
+                var extractindex = Number(getcurrentid.replace("VisualBtn", "")-1);
+                visualPopUp(extractindex);
+            } catch (error) {
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: JSON.stringify(error)
+                });
+            }
+        });
+
+        function visualPopUp(currentind) {
+            try {
+                swal.fire({
+                    title: '<strong><u>Visual Explanation</u></strong>',
+                    html:'<b>' + tableData[currentind].imageDetails + '</b>',
+                    showCloseButton: true,
+                    customClass: 'swalClass',
+                });
+
+            } catch (error) {
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong!',
+                    footer: JSON.stringify(error)
+                });
+            }
         }
 
     });
--- FrontEnd_Html5/config.js
+++ FrontEnd_Html5/config.js
@@ -4,7 +4,7 @@ requirejs.config({
     baseUrl: 'Scripts',
     paths: {
         'jquery': 'Libraries/JQuery/jquery',
-        'jqueryui': 'Libraries/JQuery/jquery-ui',
+        'jqueryui': 'Libraries/jquery-ui-1.12.1/jquery-ui',
         'sweetalert': 'Libraries/SweetAlert/sweetalert2.all',
         'datatables': 'Libraries/DataTables/datatables',
         'datatables.net': 'Libraries/DataTables/datatables.min'
