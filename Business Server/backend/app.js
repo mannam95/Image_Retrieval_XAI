98aba8ee53c21270dd72c9ff33c36d364f420c62
kushagrakumarovgu <64306534+kushagrakumarovgu@users.noreply.github.com>
2020-06-27 19:27:17 +0200

    SessionID and Session data related changes.
    Little Code cleanup is required. Will be done once integration is successful.

--- Business Server/backend/app.js
+++ Business Server/backend/app.js
@@ -5,14 +5,20 @@ const app = express();
 const axios = require("axios");
 const multer = require('multer');
 const fs = require('fs');
+var os = require("os");
+var validUrl = require('valid-url');
 
 // load configuration file.
 const conf = require('./config.json');
+const https = require('https')
 
 //map the base image absolute path to proxy resultimages directory.
 //app.use('/resultimages', express.static(path.join(conf.baseimgdir)));
 //app.use('/resultimages',express.static('E:\\SummerSem\\Project_XAI\\temporary data\\less image'));
 app.use('/resultimages',express.static(conf.staticimgpath));
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use((req, res, next) =>{
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -30,6 +36,8 @@ function checkresponse(data){
 }
 
 var httpurl = conf.protocol + "://" + conf.bserverhostip + ":" + conf.bserverportnumber 
+
+const sessiondatapath = "sessiondata"
 
 function readjsonobject(obj){
   var dirPath;
@@ -56,42 +64,147 @@ const storage = multer.diskStorage({
   }
 });
 
+
+
+async function downloadimage(url,path){
+
+  var localpath = fs.createWriteStream(path)
+
+  var request = await https.get(url,function(response){
+    
+    response.pipe(localpath)
+    console.log("--------- Downloadimage function download ---------")
+  })
+}
+/* ============================================================
+  Function: Download Image
+============================================================ */
+
+const download_image = (url, image_path) =>
+  axios({
+    url,
+    responseType: 'stream',
+  }).then(
+    response =>
+      new Promise((resolve, reject) => {
+        response.data
+          .pipe(fs.createWriteStream(image_path))
+          .on('finish', () => resolve())
+          .on('error', e => reject(e));
+        
+          console.log("download image")
+      }),
+  );
+
+
+function sendgetrequest(imagePath,req,res){
+  axios.get(imagerserverurl,{ params: {
+    file: imagePath
+  }
+  }).then(response => {
+      const data = response.data;
+      obj = JSON.parse(JSON.stringify(data));
+      // check if response data is correct.
+      if (checkresponse(data)){
+      readjsonobject(obj);
+      res.status(200).json(obj);
+    }
+    else{
+      // setting error respone code to 412. (Precondition Failed)
+      res.status(412).json(obj)
+    }
+      
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+}
+
+//}
 // Kush - fire a GET request to the Image server with image path as request param on arrival of POST request from the client.
 // It is important to set 'filename' variable before the sending the get the request.
 
 app.post('/lireq', multer({ storage: storage }).single('urld'), (req, res, next)=>{
+
+   // Kush: save the session id and session data from the incoming request.
+   //let sessionid = req.body.sessionid
+   let sessiondata = req.body.sessiondata
+   const jsonobj = JSON.parse(req.body.sessiondata);
+   //ip = ip.replace(/:/g,'')
+   let sessionid = jsonobj.sessionID.split('@')
+   console.log("sessionid: ",sessionid)
+   sessionid = sessionid[0].replace('\"','')
+   console.log("replace sessionid: ",sessionid)
+
+
+   const file = './' + sessiondatapath + '/' + sessionid
+   //console.log(jsonobj.sessionid)
+   console.log(sessiondata)
+   console.log(file)
+   fs.appendFile(file, sessiondata + os.EOL, function (err) {
+     if (err) throw err;
+     console.log('Saved!');
+   });
+   var imgurl = req.body.imgurl
+   console.log("--------->",imgurl)
+   var imagePath 
+
   if(req.file){
     filename =req.file.filename;
     imagePath = __dirname + '\\images\\' +req.file.filename;
-    
-    axios.get(imagerserverurl,{ params: {
-      file: imagePath
+    sendgetrequest(imagePath,req,res)
+  }
+  else if (imgurl.trim()) //isEmpty(imgurl)
+  {
+    console.log("==== imgurl === present")
+    if (validUrl.isUri(imgurl)) {
+      timestamp = Date.now()
+      lvpath = __dirname + '\\images\\' + 'imgdwnload' +  "-" + Date.now() +'.jpg'  ;
+      (async () => {
+          let example_image_1 = await download_image(imgurl, lvpath)
+          //console.log("Status: ",example_image_1.status)
+          //if(example_image_1.status){
+            console.log("calling get request.")
+            sendgetrequest(lvpath,req,res)
+
+          //}
+      })();
+      // downloadimage(imgurl,lvpath).then(
+      //   imagePath = lvpath
+      //   sendgetrequest(imagePath)
+      // )
+      
+      //imgparam = 1
+     } 
+    else { 
+      res.status(404).json({message:'Not valid URL to download the query image'}); 
     }
-    }).then(response => {
-        const data = response.data;
-        obj = JSON.parse(JSON.stringify(data));
-        // check if response data is correct.
-        if (checkresponse(data)){
-        readjsonobject(obj);
-        res.status(200).json(obj);
-      }
-      else{
-        // setting error respone code to 412. (Precondition Failed)
-        res.status(412).json(obj)
-      }
-        
-      })
-      .catch(error => {
-        console.log(error);
-      });
   }
 
   else{
-
-    res.status(404).json({message:'image is mandatory for this query'});
-  }  
+    res.status(404).json({message:'image or image URL is mandatory for this query'});
+  } 
+  // if (imgparam == 1)
+  // {
+  //   //console.log("image or image path is present.")
+  //   console.log("imaagepath: ",imagePath)
+  //   console.log("Sending GET request.")
+  // }
+     
 
 });
+
+// Create Session ID and send it back to the client.
+app.get('/sessionid', function (req, res) {
+  var timestamp = Date.now('milli')
+  var ip = req.ip
+  ip = ip.replace(/:/g,'')
+  ip = ip.replace(/\./g,'')
+  var sessionid = timestamp + '@' + ip
+  res.send(sessionid)
+  
+})
 
 module.exports = app;
 
